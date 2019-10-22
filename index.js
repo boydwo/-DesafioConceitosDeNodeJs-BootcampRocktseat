91cafc9e220c0adbfe3297cc0666fc0655c46d9e
boydwo <marcos.t@ufv.br>
2019-10-22 00:53:48 -0300

    editar

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const server = express();
 server.use(express.json());
 const vetProjetos = [];
 
-// adiciona
-server.post('/projects', (req, res) => {
+// adiciona 
+server.post('/projects/', (req, res) => {
   const { id } = req.body;
   const { title } = req.body;
 
@@ -16,13 +16,24 @@ server.post('/projects', (req, res) => {
 
   vetProjetos.push(projetos);
   return res.json(projetos);
+});
+
+// lista vetor
+server.get('/projects/', (req, res) => {
+  return res.json(vetProjetos);
+});
+
+// edita alterando o titulo
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const recebeArray = vetProjetos.find(item => item.id === id); // pegando elemento com id igual ao passado no parametro
+
+  recebeArray.title = title;
+
+  return res.json(recebeArray);
 })
-
-// lista
-
-
-// edita
-
 
 
 //deleta
